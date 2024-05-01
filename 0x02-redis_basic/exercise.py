749a0d125dbd298bd610be0ddb6eb57c0fe396cb
posh <mrposhboo@gmail.com>
2024-05-01 23:43:07 +0100

    cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,33 +1,56 @@
 #!/usr/bin/env python3
-
 """
-Cache module provides a class to store data in Redis cache.
+Cache module
 """
-
-import redis
 import uuid
-from typing import Union
+import redis
+from typing import Union, Callable, Optional
+from functools import wraps
 
 class Cache:
     """
-    Cache class provides methods to store data in Redis cache.
+    Cache class to store data in Redis
     """
     def __init__(self) -> None:
         """
-        Initialize a Redis client and flush the database.
+        Initialize Cache class with Redis client
         """
-        self._redis = redis.Redis()
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def call_history(method: Callable) -> Callable:
+        """
+        Decorator to store history of inputs and outputs for a function
+        """
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            input_key = "{}:inputs".format(method.__qualname__)
+            output_key = "{}:outputs".format(method.__qualname__)
+            self._redis.rpush(input_key, str(args))
+            output = method(self, *args, **kwargs)
+            self._redis.rpush(output_key, output)
+            return output
+        return wrapper
+
+    @staticmethod
+    def replay(method: Callable) -> None:
+        """
+        Display history of calls for a particular function
+        """
+        input_key = "{}:inputs".format(method.__qualname__)
+        output_key = "{}:outputs".format(method.__qualname__)
+        inputs = Cache._redis.lrange(input_key, 0, -1)
+        outputs = Cache._redis.lrange(output_key, 0, -1)
+
+        print(f"{method.__qualname__} was called {len(inputs)} times:")
+        for input_args, output in zip(inputs, outputs):
+            print(f"{method.__qualname__}{eval(input_args)} -> {output}")
+
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Store the input data in Redis using a random key.
-
-        Args:
-            data (Union[str, bytes, int, float]): The data to be stored.
-
-        Returns:
-            str: The randomly generated key used for storing the data.
+        Store data in Redis and return the key
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -35,10 +58,9 @@ class Cache:
 
 if __name__ == "__main__":
     cache = Cache()
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    cache.store("foo")
+    cache.store("bar")
+    cache.store(42)
 
+    Cache.replay(cache.store)
